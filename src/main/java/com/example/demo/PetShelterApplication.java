83c26e1f078689f1d0a53b2c9cbda22709a570be
rVencislavov1 <131446944+rVencislavov1@users.noreply.github.com>
2023-05-19 12:00:02 +0300

    Setting dependencies and project

---
+++ src/main/java/com/example/demo/PetShelterApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;u
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class PetShelterApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PetShelterApplication.class, args);
+	}
+
+}
