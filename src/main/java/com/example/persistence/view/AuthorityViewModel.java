8f3f627e0349290936bdbff66b6a82f3ddcf2871
MagicalTencho <tencho.kuyumdzhiev@blankfactor.com>
2023-06-20 14:33:01 +0300

    LoginV5

---
+++ src/main/java/com/example/persistence/view/AuthorityViewModel.java
@@ -0,0 +1,22 @@
+package com.example.persistence.view;
+
+public class AuthorityViewModel {
+    private String authority;
+
+    public AuthorityViewModel() {
+    }
+
+    public AuthorityViewModel(String authority) {
+
+        this.authority = authority;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+
+        this.authority = authority;
+    }
+}
