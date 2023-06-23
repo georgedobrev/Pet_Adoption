5a69d3058305d818da287bb86b72c04af83eef4b
dimovv23 <georgi.dimov@blankfactor.com>
2023-06-23 10:09:48 +0300

    Commits for the DEMO

--- src/main/java/com/example/controllers/HomeController.java
+++ src/main/java/com/example/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.controllers;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -14,5 +16,14 @@ public class HomeController {
     @GetMapping("/home")
     public String showHome() {
         return "index";
+    }
+
+    @GetMapping("/facebook")
+    public String getUserPage(OAuth2AuthenticationToken authentication, Model model) {
+        if (authentication != null) {
+            String name = authentication.getPrincipal().getAttribute("name");
+            model.addAttribute("name", name);
+        }
+        return "user";
     }
 }
--- src/main/java/com/example/persistence/entities/UserEntity.java
+++ src/main/java/com/example/persistence/entities/UserEntity.java
@@ -1,49 +1,39 @@
 package com.example.persistence.entities;
-
 import com.example.persistence.enums.RoleEnum;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
+import org.springframework.security.core.GrantedAuthority;
+import java.util.HashSet;
+import java.util.Set;
 @Entity
 @Getter
 @Setter
 @Table(name = "users")
 public class UserEntity {
-
     @Column(name = "user_id", nullable = true)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long userID;
-
     @Column(name = "user_first_name", nullable = false)
     private String userFirstName;
-
     @Column(name = "user_last_name", nullable = false)
     private String userLastName;
-
     @Column(name = "user_email", nullable = false)
     private String userEmail;
-
     @Column(name = "user_password", nullable = false)
     private String userPassword;
-
     @Column(name = "user_photo_url")
     private String userPhotoURL;
-
     @Column(name = "user_access_token")
     private String userAccessToken;
-
     @Column(name = "user_refresh_token")
     private String userRefreshToken;
-
-    @Column(name = "user_role", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private RoleEnum roles;
-
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "role_id"))
+    private Set<AuthorityEntity> authorities = new HashSet<>();
     public UserEntity() {
     }
-
-
 }
-
